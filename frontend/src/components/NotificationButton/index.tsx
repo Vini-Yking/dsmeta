c3388cd0ee0389ba4fcb84124e1e535ee2613862
Vini-Yking <mvdsfg@gmail.com>
2022-07-18 00:50:29 -0300

    send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,9 +1,21 @@
+import axios from 'axios';
 import icon from '../../assets/img/notificationIcon.svg'
+import { BASE_URL } from '../../utils/request';
 import './styles.css'
-const NotificationButton = () => {
+
+type Props = {
+    saleId: number;
+}
+
+const handleClick = (id: number) => {
+    axios(`${BASE_URL}/sales/${id}/notification`).then(response => {
+        console.log("sucesso")
+    })
+}
+const NotificationButton = ({ saleId }: Props) => {
 
     return (
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
             <img src={icon} alt="Notificar" />
         </div>
     )
